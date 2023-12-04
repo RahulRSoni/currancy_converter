741398150d23da748934cca62e8b69ca3447b10f
rahulRSoni <rahulsonisoni2015@gmail.com>
2023-12-04 22:28:12 +0530

    All bugs fixed

--- src/App.jsx
+++ src/App.jsx
@@ -50,7 +50,11 @@ function App() {
                   onCurrencyChange={(currency) => {
                     setAmount(currency);
                   }}
+                
                   selectCurrency={from}
+                  onAmountChange={(amount)=>{
+                    setAmount(amount)
+                  }}
                 />
               </div>
               <div className="relative w-full h-0.5">
@@ -59,7 +63,7 @@ function App() {
                   className="absolute left-1/2 -translate-x-1/2 -translate-y-1/2 border-2 border-white rounded-md bg-blue-600 text-white px-2 py-0.5"
                   onClick={swap}
                 >
-                  swap
+                  Swap
                 </button>
               </div>
               <div className="w-full mt-1 mb-4">
@@ -70,7 +74,7 @@ function App() {
                   onCurrencyChange={(currency) => {
                     setTo(currency);
                   }}
-                  selectCurrency={from}
+                  selectCurrency={to}
                   amountDisable
                 />
               </div>
--- src/componant/InputBox.jsx
+++ src/componant/InputBox.jsx
@@ -26,9 +26,7 @@ function InputBox({
           placeholder="Amount"
           disabled={amountDisable}
           value={amount}
-          onChange={(e) =>
-            onAmountChange && onAmountChange(Number(e.target.validationMessage))
-          }
+          onChange={(e) => onAmountChange && onAmountChange(Number(e.target.value))}
         />
       </div>
       <div className="w-1/2 flex flex-wrap justify-end text-right">
